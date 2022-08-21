29e52bb4628629cedfb01af2cef0c9b08043b3c1
zwsaqx <77838223+zwsaqx@users.noreply.github.com>
2022-08-22 10:50:05 +1200

    everything

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
+const App = () =>  {
+    return (
+        <Router>
+        <div>
+          <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+            <ul className="header">
+                <li><a href = "/">Home</a></li>
+                <li><a href = "/SEPractice">Select the Practice</a></li>
+                <li><a href = "/SubmitArticle">Submit an Article</a></li>
+            </ul>
+          <div className="content">
+            
+          </div>
+        </div>
+        </Router>
+    );
+}
+ 
+export default App;
