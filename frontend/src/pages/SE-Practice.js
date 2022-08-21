75cf2634596aab506e04d894220221a65af66567
zwsaqx <77838223+zwsaqx@users.noreply.github.com>
2022-08-22 11:56:59 +1200

    a

--- frontend/src/pages/SE-Practice.js
+++ frontend/src/pages/SE-Practice.js
@@ -1,14 +1,19 @@
-import React from "react";
- 
+import articles from "../dummydata/articles";
+import Styles from "../components/tablestyle.js";
+import Table from "../components/evidencetable";
+import tablecolumns from "../components/tablecolumns.js";
 const SEPractice = () => {
     return (
       <div>
         <h2>Select SE Practice to get evidence for the claimed benefits</h2>
-        <p>There will be a way to select an SE practice from our SEED repository here</p>
-        <p>Also the display table of evidence levels for different claims will show here for 
-the SE practice selected</p>
+               <Styles>
+                 <Table
+                  data={articles}
+                  columns={tablecolumns}
+                 />
+              </Styles>
       </div>
     );
 }
  
-export default SEPractice;
+export default SEPractice;  
