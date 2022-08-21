4b8aac27c756c563be70bf02e6894bce3aa131f9
zwsaqx <77838223+zwsaqx@users.noreply.github.com>
2022-08-22 11:17:20 +1200

    working!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,33 @@
 import React from "react";
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Redirect
+} from "react-router-dom";
+ 
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
 import NotFoundPage from "./pages/404";
+
+
 const App = () =>  {
     return (
         <Router>
         <div>
-          <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+          <h1>Software Engineering Empirical Evidence Database (SEED)</h1>
             <ul className="header">
-                <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
+                <li><NavLink exact to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
             </ul>
           <div className="content">
-            
+            <Route exact path="/" component={Home}/>
+            <Route  path="/SEPractice" component={SEPractice}/>
+            <Route  path="/SubmitArticle" component={SubmitArticle}/>
+            <Route exact path="/404" component={NotFoundPage}/>
+            <Redirect to="/404" />
           </div>
         </div>
         </Router>
